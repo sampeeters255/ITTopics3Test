1dcce8a1f552ade9509eee3de4b715884c048e6f
sampeeters255 <74966017+sampeeters255@users.noreply.github.com>
2021-10-22 13:16:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItTopics3TestGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
